b7d15454b33b1f3aac3c045d6543393584f429f4
Jan Steffen <jan.steffen@bwi.de>
2026-01-14 12:57:06 +0100

    fix stuff

--- pkg/discovery/catalogr/helm_handler.go
+++ pkg/discovery/catalogr/helm_handler.go
@@ -1,1 +1,4 @@
+// Copyright 2026 BWI GmbH and Artifact Conduit contributors
+// SPDX-License-Identifier: Apache-2.0
+
 package catalogr
--- test/helpers.go
+++ test/helpers.go
@@ -1,3 +1,6 @@
+// Copyright 2026 BWI GmbH and Artifact Conduit contributors
+// SPDX-License-Identifier: Apache-2.0
+
 package test
 
 import (
