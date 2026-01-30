c01a574d61ac5019c56f41bb076de3b2aa4d063e
Alexander Olzem <alexander.olzem@bwi.de>
2026-01-30 14:42:13 +0100

    Add first iteration of hydrated-target

---
+++ pkg/renderer/render_hydrated_target_test.go
@@ -0,0 +1,42 @@
+package renderer
+
+import (
+	"encoding/json"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("RenderHydratedTarget", func() {
+	var (
+		result *RenderResult
+		err    error
+	)
+
+	AfterEach(func() {
+		if result != nil {
+			err := result.Close()
+			Expect(err).NotTo(HaveOccurred())
+		}
+	})
+
+	Describe("RenderHydratedTarget with valid HydratedTargetConfig", func() {
+		It("should render without errors", func() {
+			config := HydratedTargetConfig{
+				Chart: ChartConfig{
+					Name:        "test-hydrated-target",
+					Description: "Test HydratedTarget Chart",
+					Version:     "1.0.0",
+					AppVersion:  "1.0.0",
+				},
+				Input:  HydratedTargetInput{},
+				Values: json.RawMessage(`{}`),
+			}
+
+			result, err = RenderHydratedTarget(config)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).NotTo(BeNil())
+			Expect(result.Dir).NotTo(BeEmpty())
+		})
+	})
+})
