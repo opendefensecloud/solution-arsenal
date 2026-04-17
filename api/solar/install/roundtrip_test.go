da38e8026e8756080a77dd000d14b0ecac58f74c
Moritz Bracht <moritz.bracht@opendefense.cloud>
2026-04-17 10:09:22 +0200

    fix: pipeline errors

--- api/solar/install/roundtrip_test.go
+++ api/solar/install/roundtrip_test.go
@@ -7,11 +7,14 @@ import (
 	"testing"
 
 	"k8s.io/apimachinery/pkg/api/apitesting/roundtrip"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	"go.opendefense.cloud/solar/api/solar/fuzzer"
 )
 
 func TestRoundTripTypes(t *testing.T) {
+	scheme := runtime.NewScheme()
+	Install(scheme)
 	roundtrip.RoundTripTestForAPIGroup(t, Install, fuzzer.Funcs)
 	// TODO: enable protobuf generation for the sample-apiserver
 	// roundtrip.RoundTripProtobufTestForAPIGroup(t, Install, orderfuzzer.Funcs)
