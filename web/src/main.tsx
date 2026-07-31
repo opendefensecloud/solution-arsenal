51dff8ec9eba851775677fbe15f8e77cb44a3c01
Elisa <elisa.oezdemir2@gmail.com>
2026-07-31 14:08:06 +0200

    fix: e2e test regression introduced with discovery api watch

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -21,7 +21,7 @@ const queryClient = new QueryClient({
 
 declare global {
   interface Window {
-    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient
+    __TANSTACK_QUERY_CLIENT__: QueryClient
   }
 }
 
